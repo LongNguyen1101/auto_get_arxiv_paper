7959ebff5a31c3984171c93bdd41571799ee0dd4
LongNguyen1101 <longnguyenhoang1101.work@gmail.com>
2025-03-16 10:04:47 +0700

    Change cron and config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ def get_config():
         'search_query': "cat:cs.AI",
         'sortBy': "submittedDate",
         'sortOrder': "descending",
-        'max_results': 5,
+        'max_results': 10,
         'url': "http://export.arxiv.org/api/query",
     }
